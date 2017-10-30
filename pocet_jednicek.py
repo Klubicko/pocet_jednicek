1af32bec7f73760f69058d441de6195815627c7d
Ewa Szczurowska <szczurowska.e@gmail.com>
2017-10-30 20:12:08 +0100

    ymena na jednicku

--- pocet_jednicek.py
+++ pocet_jednicek.py
@@ -1,8 +1,24 @@
-#program, ktery nacte cele cislo a spocita pocet jednicek
+### Spocitej pocet jednicek v zadanem cisle
+# def pocet_jednicek(cislo):
+#     pocet = 0
+#     while cislo >0:
+#         zbytek = cislo %10
+#         if zbytek == 1:
+#             pocet = pocet +1
+#         cislo = cislo // 10
+#     return pocet
+#
+# cislo= int(input('insert number:'))
+# pocet = pocet_jednicek(cislo)
+#
+# print ("pocet jednicek v cisle {} je {}.".format(cislo,pocet))
+
 
 def pocet_jednicek(cislo):
-    pocet = 0
-    while cislo > 0:
+    # pocet = 0
+    # if cilslo < 0
+    cislo=-cislo
+    while abs (cislo) > 1:
         zbytek = cislo % 10
         if zbytek == 1:
             pocet = pocet + 1
@@ -12,9 +28,3 @@ def pocet_jednicek(cislo):
 cislo = input('Zadej cislo: ')
 pocet = pocet_jednicek(cislo)
 print('Pocet jednicek v cisle {} je {}.'.format(cislo, pocet))
-
-#cislo = input('Zadej cislo: ')
-#
-# pocet_jednicek = cislo.count('1')
-#
-# print('Pocet jednicek v cisle {} je {}.'.format(cislo, pocet_jednicek))
